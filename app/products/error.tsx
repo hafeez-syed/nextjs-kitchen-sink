1404c415bb357cda7fc6103a75ce737bcdb21ca1
Hafeez Syed <hafeez-syed@users.noreply.github.com>
2024-07-04 04:07:57 +0000

    custom error for global and internal routes

--- app/products/error.tsx
+++ app/products/error.tsx
@@ -3,7 +3,7 @@
 export default function Error({ error, reset }: any) {
 	return (
 		<>
-			<h1>Error: Global ERROR: {error.message}</h1>
+			<h1>Error: Local ERROR for PRODUCTS: {error.message}</h1>
 			<button onClick={reset}>Reset</button>
 		</>
 	);
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,29 +1,29 @@
 import type { Metadata } from "next";
 
 export async function generateMetadata({ params }: any): Promise<Metadata> {
-  // fetch request here to extract dynamic content
-  // const response = await fetch()
+	// fetch request here to extract dynamic content
+	// const response = await fetch()
 
-  return {
-    title: `Product ${params.id}, is known as "Fetched Product Name from the API"`,
-  };
+	return {
+		title: `Product ${params.id}, is known as "Fetched Product Name from the API"`,
+	};
 }
 
 const fetchProducts = async () => {
-  // throw new Error("Oops! Something went wrong");
-  await wait(2000);
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  return await response.json();
+	throw new Error("Oops! Something went wrong");
+	await wait(2000);
+	const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+	return await response.json();
 };
 
 export default async function Products() {
-  const products = await fetchProducts();
-  return (
-    <>
-      <h1>Products</h1>
-      {products.length}
-    </>
-  );
+	const products = await fetchProducts();
+	return (
+		<>
+			<h1>Products</h1>
+			{products.length}
+		</>
+	);
 }
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
